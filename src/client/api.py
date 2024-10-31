038c3799bd3a700cfb5beccd1568f34d1ec1c6c0
Chun-Cheng Yu <61136202+Chun-Cheng@users.noreply.github.com>
2024-10-31 22:24:23 +0800

    refactor: modify import statements

--- src/client/api.py
+++ src/client/api.py
@@ -1,61 +1,62 @@
 from datetime import datetime
 
 import grpc
-from proto_gen.chat import message_pb2
-from proto_gen.chat import room_pb2
-from proto_gen.chat import user_pb2
-from proto_gen.chat import service_pb2
-from proto_gen.chat import service_pb2_grpc
-from google.protobuf import timestamp_pb2
+from proto_gen.chat.message_pb2 import Message
+from proto_gen.chat.room_pb2 import Room
+from proto_gen.chat.user_pb2 import User
+from proto_gen.chat.service_pb2 import UserRequest, RoomRequest, JoinRoomRequest
+from proto_gen.chat.service_pb2_grpc import ServiceStub
+from google.protobuf.timestamp_pb2 import Timestamp
 
 import db as db
 
 channel = grpc.insecure_channel('localhost:3000')
-stub = service_pb2_grpc.ServiceStub(channel)
+stub = ServiceStub(channel)
 
 def get_username() -> str | None:
     result = db.get_username()
     return result if result else None
 
 def signup(username: str) -> None:
-    stub.CreateUser(user_pb2.User(username=username, password="", room_ids=[]))
+    stub.CreateUser(User(username=username, password="", room_ids=[]))
     db.set_username(username)
 
 def get_rooms(username: str = db.get_username()) -> list:
-    return [
-        room for room in stub.GetUser(service_pb2.UserRequest(username=username)).room_ids
-    ]
-    # TODO: update local db
+    rooms = stub.GetUser(UserRequest(username=username)).room_ids
+    db.update_rooms(rooms)
+    return rooms
 
 def create_room(room_name: str) -> None:
-    stub.CreateRoom(room_pb2.Room(room_id="", name=room_name, user_ids=[get_username()]))
+    stub.CreateRoom(Room(room_id="", name=room_name, user_ids=[get_username()]))
 
 def invite_user(room_id: str, username: str) -> None:
-    room = stub.GetRoom(service_pb2.RoomRequest(room_id=room_id))
-    stub.JoinRoom(service_pb2.JoinRoomRequest(room_id=room.room_id, user_id=username))
+    room = stub.GetRoom(RoomRequest(room_id=room_id))
+    stub.JoinRoom(JoinRoomRequest(room_id=room.room_id, user_id=username))
 
 def send_message(room_id: str, message: str) -> None:
-    msg = message_pb2.Message(
+    timestamp=Timestamp()
+    timestamp.FromDatetime(datetime.now())
+    msg = Message(
         message_id="",
         author_id=db.get_username(),
         room_id=room_id,
         text=message,
-        timestamp=timestamp_pb2.Timestamp().FromDatetime(datetime.now())
+        timestamp=timestamp
     )
     stub.SendMessage(msg)
     db.insert_message(msg.message_id, msg.author_id, msg.room_id, msg.text, datetime.fromtimestamp(msg.timestamp.seconds + msg.timestamp.nanos/1e9))
 
 def join_room(room_id: str, username: str = db.get_username()) -> None:
-    room = stub.GetRoom(service_pb2.RoomRequest(room_id=room_id)) # TODO: search in local db
-    stub.JoinRoom(service_pb2.JoinRoomRequest(room_id=room.room_id, user_id=username))
-    # db.join_room(room.room_id)
+    room = stub.GetRoom(RoomRequest(room_id=room_id))
+    stub.JoinRoom(JoinRoomRequest(room_id=room.room_id, user_id=username))
+    db.join_room(room.room_id)
 
 async def get_new_messages(room_id: str) -> None:
-    for message in stub.GetMessages(service_pb2.UserRequest(username=db.get_username())):
+    for message in stub.GetMessages(UserRequest(username=db.get_username())):
         db.insert_message(message.message_id, message.author_id, room_id, message.text, message.timestamp)
 
 
-#
+
 # stub.CreateUser(user_pb2.User())
 
 # user = stub.GetUser(user_pb2.User())
--- src/server/server.py
+++ src/server/server.py
@@ -4,55 +4,55 @@ from datetime import datetime
 from typing import Any
 
 import grpc
-from proto_gen.chat import message_pb2
-from proto_gen.chat import room_pb2
-from proto_gen.chat import user_pb2
-from proto_gen.chat import service_pb2
-from proto_gen.chat import service_pb2_grpc
+from proto_gen.chat.message_pb2 import Message
+from proto_gen.chat.room_pb2 import Room
+from proto_gen.chat.user_pb2 import User
+from proto_gen.chat.service_pb2 import UserRequest, RoomRequest, JoinRoomRequest
+from proto_gen.chat.service_pb2_grpc import ServiceServicer, add_ServiceServicer_to_server
 
-from google.protobuf import empty_pb2
+from google.protobuf.empty_pb2 import Empty
 # from google.protobuf.timestamp_pb2 import Timestamp
 
 import db as db
 
 
-class ServiceServicer(service_pb2_grpc.ServiceServicer):
-    def CreateUser(self: Any, request: service_pb2.UserRequest, unused_context) -> empty_pb2.Empty:
+class ServiceServicer(ServiceServicer):
+    def CreateUser(self: Any, request: UserRequest, unused_context) -> Empty:
         logging.info(f"Creating user {request.username}")
         db.new_user(request.username, "")
-        return empty_pb2.Empty()
+        return Empty()
 
-    def GetUser(self: Any, request: service_pb2.UserRequest, unused_context) -> user_pb2.User:
+    def GetUser(self: Any, request: UserRequest, unused_context) -> User:
         logging.info(f"Getting user {request.username}")
         user = db.get_user(request.username)
-        return user_pb2.User(username=user["username"], password="", room_ids=user["rooms"])
-        return user_pb2.User(
+        return User(username=user["username"], password="", room_ids=user["rooms"])
+        return User(
             username="test_user_1", 
             password="", 
             room_ids=["test_room"]
         )
 
-    def CreateRoom(self: Any, request: room_pb2.Room, unused_context) -> empty_pb2.Empty:
+    def CreateRoom(self: Any, request: Room, unused_context) -> Empty:
         logging.info(f"Creating room {request.name}")
         db.new_room(request.name, request.user_ids[0])
-        return empty_pb2.Empty()
+        return Empty()
     
-    def JoinRoom(self: Any, request: service_pb2.JoinRoomRequest, unused_context) -> empty_pb2.Empty:
+    def JoinRoom(self: Any, request: JoinRoomRequest, unused_context) -> Empty:
         logging.info(f"Joining room {request.room_id}")
         db.join_room(request.user_id, request.room_id)
-        return empty_pb2.Empty()
+        return Empty()
 
-    def GetRoom(self: Any, request: service_pb2.RoomRequest, unused_context) -> room_pb2.Room:
+    def GetRoom(self: Any, request: RoomRequest, unused_context) -> Room:
         logging.info(f"Getting room {request.room_id}")
         room = db.get_room(request.room_id)
-        return room_pb2.Room(room_id=room["room_id"], name=room["name"], user_ids=room["user_id"])
-        return room_pb2.Room(
+        return Room(room_id=room["room_id"], name=room["name"], user_ids=room["user_id"])
+        return Room(
             room_id="test_room_id", 
             name="test_room", 
             user_ids=["test_user_1", "test_user_2"]
         )
 
-    def SendMessage(self: Any, request: message_pb2.Message, unused_context) -> empty_pb2.Empty:
+    def SendMessage(self: Any, request: Message, unused_context) -> Empty:
         logging.info("Sending message")
         server_time = datetime.now()
         user_timestamp = request.timestamp
@@ -64,14 +64,14 @@ class ServiceServicer(service_pb2_grpc.ServiceServicer):
             msg_time = server_time
 
         db.new_message(request.author_id, request.room_id, request.text, msg_time)
-        return empty_pb2.Empty()
+        return Empty()
 
-    async def GetMessages(self: Any, request: service_pb2.UserRequest, unused_context):
+    async def GetMessages(self: Any, request: UserRequest, unused_context):
         # get user new messages
         while True:
             for message in db.get_user_messages(request.username):
                 logging.info(f"Sending message {message['msg_id']}")
-                yield message_pb2.Message(
+                yield Message(
                     message_id=message["msg_id"], 
                     author_id=message["author_user_id"], 
                     room_id=message["room_id"], 
@@ -85,7 +85,7 @@ async def serve() -> None:
     db.init_db()
     # server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     server = grpc.aio.server()
-    service_pb2_grpc.add_ServiceServicer_to_server(ServiceServicer(), server)
+    add_ServiceServicer_to_server(ServiceServicer(), server)
     server.add_insecure_port("[::]:3000")
     await server.start()
     await server.wait_for_termination()
